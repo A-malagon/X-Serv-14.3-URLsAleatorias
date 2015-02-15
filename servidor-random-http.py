6cf99c45fb38a3a904b81b73e44bf14c89b7e874
A-malagon <alejanm09@gmail.com>
2015-02-15 20:21:25 +0100

    URLs Aleatorias

---
+++ servidor-random-http.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Servidor HTTP Simple con url aleatoria : Alejandro Malagón López-Páez
+"""
+
+import random
+import socket
+
+# Create a TCP objet socket and bind it to a port
+# Port should be 80, but since it needs root privileges,
+# let's use one above 1024
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Let the port be reused if no process is actually using it
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Bind to the address corresponding to the main name of the host
+mySocket.bind((socket.gethostname(), 1234))
+
+# Queue a maximum of 5 TCP connection requests
+
+mySocket.listen(5)
+
+# Accept connections, read incoming data, and answer back an HTML page
+#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
+
+try:
+    while True:
+        numeroAleatorio = random.randint(0, 500000000)
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        print 'Request received:'
+        print recvSocket.recv(2048)
+        print 'Answering back...'
+        recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                        "<html><body><h1>Hola</h1>" +
+                        "\r\n" +
+                        "<a href='" + str(numeroAleatorio) + "'>Dame otra</a>"
+                        "</body></html>" +
+                        "\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print "Closing binded socket"
+    mySocket.close()
